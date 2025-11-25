d2e244458f6f394c28685652fe3715e4f096bbfc
NarEsguerra <127297607+NarEsguerra@users.noreply.github.com>
2025-11-25 06:54:31 +0000

    Latest Update

--- server.js
+++ server.js
@@ -87,8 +87,6 @@ const portfolioHTML = `
 </html>
 `;
 
-const http = require("http");
-
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/html" });
   res.end(portfolioHTML);
